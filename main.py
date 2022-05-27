d7d4725fdc499c271b284130bee73505a7b7fd1d
Sukith S <95138647+Suki1913@users.noreply.github.com>
2022-05-27 19:54:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ mv=movie.cursor()
 
 
 #Creating "movies" table
-# mv.execute('''CREATE TABLE Movies (
-#     name TEXT PRIMARY KEY NOT NULL,
-#     actor TEXT,
-#     actress TEXT,
-#     director TEXT NOT NULL,
-#     year_of_release INT NOT NULL );''')
+mv.execute('''CREATE TABLE Movies (
+    name TEXT PRIMARY KEY NOT NULL,
+    actor TEXT,
+    actress TEXT,
+    director TEXT NOT NULL,
+    year_of_release INT NOT NULL );''')
 
 
 
 #Inserting data into Movies table
-# mv.execute("INSERT INTO Movies VALUES('Inception','Leonardo DiCaprio','Marion Cotillard','Christopher Nolan',2010);")
-# mv.execute("INSERT INTO Movies VALUES('Interstellar','Matthew McConaughey','Anne Hathaway','Christopher Nolan',2014);")
-# mv.execute("INSERT INTO Movies VALUES('Jumanji: The Next Level','Dwayne Johnson','Karen Gillan','Jake Kasdan',2019);")
-# mv.execute("INSERT INTO Movies VALUES('Central Intelligence','Dwayne Johnson','','Rawson Marshall Thurber',2016);")
-# mv.execute("INSERT INTO Movies VALUES('Lucy','','Scarlett Ingrid Johansson','Luc Besson',2014);")
-# mv.execute("INSERT INTO Movies VALUES('The Fast and the Furious: Tokyo Drift','Lucas Black','Nathalie Kelley','Justin Lin',2006);")
+mv.execute("INSERT INTO Movies VALUES('Inception','Leonardo DiCaprio','Marion Cotillard','Christopher Nolan',2010);")
+mv.execute("INSERT INTO Movies VALUES('Interstellar','Matthew McConaughey','Anne Hathaway','Christopher Nolan',2014);")
+mv.execute("INSERT INTO Movies VALUES('Jumanji: The Next Level','Dwayne Johnson','Karen Gillan','Jake Kasdan',2019);")
+mv.execute("INSERT INTO Movies VALUES('Central Intelligence','Dwayne Johnson','','Rawson Marshall Thurber',2016);")
+mv.execute("INSERT INTO Movies VALUES('Lucy','','Scarlett Ingrid Johansson','Luc Besson',2014);")
+mv.execute("INSERT INTO Movies VALUES('The Fast and the Furious: Tokyo Drift','Lucas Black','Nathalie Kelley','Justin Lin',2006);")
 
 
 
@@ -32,13 +32,13 @@ mv=movie.cursor()
 #Querying data from Movies table
 
 #Retrieve all data from table
-# mv.execute("SELECT * FROM movies;")  
-# print(mv.fetchall())
+mv.execute("SELECT * FROM movies;")  
+print(mv.fetchall())
 
 
 #Retrieve all movies name starred by actor Dwayne Johnson
-# mv.execute("SELECT name FROM movies WHERE actor='Dwayne Johnson';")
-# print(mv.fetchall())
+mv.execute("SELECT name FROM movies WHERE actor='Dwayne Johnson';")
+print(mv.fetchall())
 
 
 #Retrieve movies realesed between 2015 to 2020 in ascending order of year of release
